9a33fc258011490204802b9377ce44a1b807d7ae
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-10-03 02:53:26 +0300

    Add files via upload

---
+++ TradingCompany/TradingCompany/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["TradingCompany"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connStr);
+            try
+            {
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            Console.Read();
+        }
+    }
+}
